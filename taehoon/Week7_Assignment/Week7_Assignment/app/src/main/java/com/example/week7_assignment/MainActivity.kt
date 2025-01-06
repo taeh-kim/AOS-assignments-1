7f02f29b57d9cda070479f939e7bd427c1d1a731
kim_taehoon <kim.taehoon2001@gmail.com>
2025-01-06 19:16:27 +0900

    Week7_Assignment_2

--- taehoon/Week7_Assignment/Week7_Assignment/app/src/main/java/com/example/week7_assignment/MainActivity.kt
+++ taehoon/Week7_Assignment/Week7_Assignment/app/src/main/java/com/example/week7_assignment/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.week7_assignment
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -11,6 +12,7 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.lifecycleScope
@@ -52,7 +54,12 @@ class MainActivity : ComponentActivity() {
 fun MyScreen() {
     var message by remember { mutableStateOf("Corutine Result : Idle") } // 초기값 설정
 
-    Column(modifier = Modifier.padding(16.dp)) {
+    Column(modifier = Modifier
+        .padding(16.dp)
+        .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         Text(text = message, modifier = Modifier.padding(32.dp))
         Button(onClick = {
             message = "Corutine Result : Loading..."
